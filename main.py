b18015854b0c958ef4934e91499c3f0d61be877f
francesco-ftk <francesco.fantechi3@stud.unifi.it>
2023-05-04 08:05:48 +0200

    wip fixing multiprocessing

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import datetime
 import time
 from joblib import Parallel, delayed, dump, load
 
-TRANSFORMATIONS = 100
+TRANSFORMATIONS = 1000
 
 
 def data_augmentation(foreground, backgrounds, transformations) -> int:
@@ -53,10 +53,47 @@ def data_augmentation(foreground, backgrounds, transformations) -> int:
     return 0
 
 
-def data_augmentation_multiprocessing(foreground, backgrounds, dir_path, count):
-    index = random.randint(0, len(backgrounds) - 1)
-    background = copy(backgrounds[index])
+def data_augmentation_multiprocessing(foreground, backgrounds, dir_path, trasformations):
+    for i in range(0, trasformations, 1):
+        index = random.randint(0, len(backgrounds) - 1)
+        background = copy(backgrounds[index])
 
+        if background.shape[0] < foreground.shape[0] or background.shape[1] < foreground.shape[1]:
+            print("ERROR: the foreground exceeds the background dimensions")
+            return 1
+
+        if background.shape[0] - foreground.shape[0] == 0:
+            row = 0
+        else:
+            row = random.randint(0, background.shape[0] - foreground.shape[0] - 1)
+
+        if background.shape[1] - foreground.shape[1] == 0:
+            col = 0
+        else:
+            col = random.randint(0, background.shape[1] - foreground.shape[1] - 1)
+
+        alpha = random.randint(128, 255)
+        alpha_correction_factor = float(alpha) / 255.0
+        beta_correction_factor = 1.0 - alpha_correction_factor
+
+        for j in range(0, foreground.shape[0], 1):
+            for k in range(0, foreground.shape[1], 1):
+                f_pixel = foreground[j, k]
+                b_pixel = background[row + j, col + k]
+                f_alpha = float(f_pixel[3]) / 255.0
+                if f_alpha > 0.9:
+                    background[row + j, col + k] = [float(b_pixel[0]) * beta_correction_factor + float(
+                        f_pixel[0]) * alpha_correction_factor * f_alpha,
+                                                    float(b_pixel[1]) * beta_correction_factor + float(
+                                                        f_pixel[1]) * alpha_correction_factor * f_alpha,
+                                                    float(b_pixel[2]) * beta_correction_factor + float(
+                                                        f_pixel[2]) * alpha_correction_factor * f_alpha, 255]
+
+        cv2.imwrite(dir_path + "/quokka_" + str(i) + "_" + str(i) + ".png", background)  # TODO first i = nome thread
+
+
+def data_augmentation_multiprocessing_one_at_a_time(foreground, background_img, dir_path, count):
+    background = copy(background_img)
     if background.shape[0] < foreground.shape[0] or background.shape[1] < foreground.shape[1]:
         print("ERROR: the foreground exceeds the background dimensions")
         return 1
@@ -92,6 +129,8 @@ def data_augmentation_multiprocessing(foreground, backgrounds, dir_path, count):
 
 
 if __name__ == '__main__':
+
+    # LOAD THE IMAGES
     background_path = "input/background/"
     foreground_path = "input/foreground/"
 
@@ -112,44 +151,37 @@ if __name__ == '__main__':
     else:
         print("ERROR: foreground not found at: " + foreground_path)
 
-    print("START Sequential Algorithm")
+    # SEQUENTIAL IMAGE COMPOSITION
+    # print("START Sequential Algorithm")
+    #
+    # start = time.time()
+    # result = data_augmentation(foreground, backgrounds, TRANSFORMATIONS)
+    # end = time.time()
+    #
+    # if result == 0:
+    #     print("Program Sequential Successfully Completed")
+    # else:
+    #     print("Program Sequential Failed")
+    #
+    # print(f'Sequential running took {end - start} seconds.')
 
-    start = time.time()
-    result = data_augmentation(foreground, backgrounds, TRANSFORMATIONS)
-    end = time.time()
-
-    if result == 0:
-        print("Program Sequential Successfully Completed")
-    else:
-        print("Program Sequential Failed")
-
-    print(f'Sequential running took {end - start} seconds.')
-
-    # folder = './joblib_memmap'
-    # try:
-    #     os.mkdir(folder)
-    # except FileExistsError:
-    #     pass
-    # background_filename_memmap = os.path.join(folder, 'backgrounds_memmap')
-    # dump(backgrounds, background_filename_memmap)
-    # backgrounds = load(background_filename_memmap, mmap_mode='r')
-
-    # foreground_filename_memmap = os.path.join(folder, 'foreground_memmap')
-    # dump(foreground, foreground_filename_memmap)
-    # foreground = load(foreground_filename_memmap, mmap_mode='r')
-
+    # MULTIPROCESSING IMAGE COMPOSITION
     print("START Multiprocessing Algorithm")
 
     start = time.time()
     local_date = datetime.datetime.now()
     new_dir_path = 'output/' + str(local_date)
     os.mkdir(new_dir_path)
+    index = random.randint(0, len(backgrounds) - 1)
+    background = backgrounds[index]
     Parallel(n_jobs=12)(
-        delayed(data_augmentation_multiprocessing)(foreground, backgrounds, new_dir_path, i) for i in
-        range(TRANSFORMATIONS))
+        delayed(data_augmentation_multiprocessing_one_at_a_time)(foreground, background, new_dir_path, i) for i
+        in range(TRANSFORMATIONS))
     end = time.time()
 
     print(f'Multiprocessing Running took {end - start} seconds.')
 
-
-# TODO una immagine alla volta di background, 1000 trasformazioni, multiprocessing manualmente con trasformazioni/5
+# TODO una immagine alla volta di background, NO
+#  1000 trasformazioni, NO
+#  multiprocessing manualmente con trasformazioni/5,
+#  profiling
